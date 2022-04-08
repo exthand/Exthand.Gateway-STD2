679f0467e748b01c78b820828c5661d2c6d57b2f
Jean-Gabriel Debaille <jeangabriel@exthand.com>
2022-04-08 11:56:27 +0200

    V 3.0.5

---
+++ ExthandGateway/GatewayConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Exthand.GatewayClient.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Exthand.GatewayClient
+{
+    public static class GatewayConfigurator
+    {
+        public static void AddGatewayService(this IServiceCollection services, Uri serverUrl, int httpClientTimeoutMilliseconds = 100000)
+        {
+            services.AddHttpClient("BankingSdkGatewayClient", c =>
+            {
+                c.BaseAddress = serverUrl;
+                c.Timeout = TimeSpan.FromMilliseconds(httpClientTimeoutMilliseconds);
+            });
+            services.AddScoped<IGatewayService, GatewayService>();
+        }
+    }
+}
